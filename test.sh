af60c69762498eff0eb62e74f1ef3ab276f65137
wij <41879253+wijiler@users.noreply.github.com>
2024-08-11 11:48:23 -0400

    fixed bug with add_ident that caused tokens not to register, started working on parsing

---
+++ test.sh
@@ -0,0 +1,6 @@
+jai first.jai
+.\\FIEN.exe .\\examples\\parsertest.fien
+echo "============================="
+.\\FIEN.exe .\\examples\\struct.fien
+echo "============================="
+.\\FIEN.exe .\\examples\\testing.fien
