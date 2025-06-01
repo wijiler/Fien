2c0127b0ee1619b9ecd834d74100a9e400490de2
wij <41879253+wijiler@users.noreply.github.com>
2025-06-01 11:21:46 -0400

    Add enum node, and optimize ASTNode size

--- Fien-Frontend/include/ast.hpp
+++ Fien-Frontend/include/ast.hpp
@@ -45,6 +45,83 @@ namespace Fien
     typedef struct Ast Ast;
     struct AstNode
     {
+        union NodeData
+        {
+            struct ExprNode
+            {
+                void *data;
+            };
+            ExprNode expr;
+            struct ScopeEntryNode
+            {
+                char *name;
+            };
+            ScopeEntryNode scope_entry;
+            struct BinOpExprNode
+            {
+                ExprNode rhs, lhs;
+                uint8_t kind;
+            };
+            BinOpExprNode bin_op_expr;
+            struct UniOpExprNode
+            {
+                ExprNode expression;
+                OperationKind kind;
+            };
+            UniOpExprNode uni_op_expr;
+            struct DeclarationNote
+            {
+                char *data;
+            };
+            struct DeclarationNode
+            {
+                char *ident;
+                DeclarationNote *note; // nullptr if no notes
+                ExprNode value;
+                ValidTypes type;
+                uint8_t is_const;
+            };
+            DeclarationNode declaration;
+            struct CodeBlockNode
+            {
+                AstNode *children;
+                CodeBlockNode *parent_block; // nullptr if global scope
+                ScopeEntryNode *scope;       // nullptr if global scope (will be your parent block)
+                uint64_t child_count;
+            };
+            CodeBlockNode code_block;
+            struct IfNode
+            {
+                CodeBlockNode code;
+                CodeBlockNode *else_statement; // nullptr if no else
+                ExprNode conditional;
+            };
+            IfNode if_statement;
+            struct StructNode
+            {
+                CodeBlockNode block;
+                DeclarationNote *note; // nullptr if no note
+            };
+            StructNode struct_node;
+            struct EnumNode
+            {
+                DeclarationNode *members; // empty enum is error, always full, always constant
+                DeclarationNote *note;    // nullptr if no note
+                char *name;
+                uint64_t member_count;
+            };
+            struct ProcNode
+            {
+                CodeBlockNode block;
+                DeclarationNode *args;
+                DeclarationNote *note; // nullptr if no note
+                char *ident;
+                uint8_t arg_count; // you're genuinely sick if you have more than 256 args and dont combine it into a struct type
+            };
+            ProcNode procedure;
+            ~NodeData();
+        } data;
+
         enum class NodeKind : uint8_t
         {
             Expression = 0,
@@ -56,76 +133,8 @@ namespace Fien
             If,
             Else,
             Struct,
+            Enum,
         } kind;
-        union NodeData
-        {
-            struct ExprNode
-            {
-                void *data;
-            };
-            ExprNode expr;
-            struct ScopeEntryNode
-            {
-                std::string name;
-            };
-            ScopeEntryNode scope_entry;
-            struct BinOpExprNode
-            {
-                uint8_t kind;
-                ExprNode rhs, lhs;
-            };
-            BinOpExprNode bin_op_expr;
-            struct UniOpExprNode
-            {
-                OperationKind kind;
-                ExprNode expression;
-            };
-            UniOpExprNode uni_op_expr;
-            struct DeclarationNote
-            {
-                std::string data;
-            };
-            struct DeclarationNode
-            {
-                std::string ident;
-                DeclarationNote *note; // nullptr if no notes
-                ExprNode value;
-                ValidTypes type;
-                uint8_t is_const;
-            };
-            DeclarationNode declaration;
-            struct CodeBlockNode
-            {
-                CodeBlockNode *parent_block; // nullptr if global scope
-                ScopeEntryNode *scope;       // nullptr if global scope (will be your parent block)
-                AstNode *children;
-                uint64_t child_count;
-            };
-            CodeBlockNode code_block;
-            struct IfNode
-            {
-                ExprNode conditional;
-                CodeBlockNode code;
-                CodeBlockNode else_statement;
-            };
-            IfNode if_statement;
-            struct StructNode
-            {
-                CodeBlockNode block;
-                DeclarationNote *note; // nullptr if no notes
-            };
-            StructNode struct_node;
-            struct ProcNode
-            {
-                std::string ident;
-                DeclarationNode *args;
-                CodeBlockNode block;
-                DeclarationNote *note; // nullptr if no notes
-                uint8_t arg_count;     // you're genuinely sick if you have more than 256 args and dont combine it into a struct type
-            };
-            ProcNode procedure;
-            ~NodeData();
-        } data;
     };
 
     struct AstError
